170f4d4af04c627e4f9d7c89c436c4dd4202e5ab
RenHeyzer <vesolyyradin@gmail.com>
2024-04-20 20:05:14 +0600

    removed binding

--- app/src/main/java/com/geeks/githubandroid15/MainActivity.kt
+++ app/src/main/java/com/geeks/githubandroid15/MainActivity.kt
@@ -6,11 +6,7 @@ import com.geeks.githubandroid15.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
     }
 }
