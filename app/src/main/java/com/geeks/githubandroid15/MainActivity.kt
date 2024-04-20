0b4a3993dd88c35cb8bb85228a5363c510dd0c30
RenHeyzer <vesolyyradin@gmail.com>
2024-04-20 20:09:59 +0600

    Revert "removed binding"
    This reverts commit 170f4d4af04c627e4f9d7c89c436c4dd4202e5ab.

--- app/src/main/java/com/geeks/githubandroid15/MainActivity.kt
+++ app/src/main/java/com/geeks/githubandroid15/MainActivity.kt
@@ -6,7 +6,11 @@ import com.geeks.githubandroid15.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
